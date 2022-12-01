e1fcd9283e7e6659970f8f37d45737462d2a6372
Osariemen Daniel Osazee <63324071+kyilax@users.noreply.github.com>
2022-12-01 02:56:33 +0100

    Update XContext.js

--- frontend/context/XContext.js
+++ frontend/context/XContext.js
@@ -14,7 +14,7 @@ export const XProvider = ({ children }) => {
   const router = useRouter()
 
   const xtelptAddress = "0x571eefb0C86b8d281DF6C6520625BC86569cC60b"
-  const nftAddress = "0x6fA103f522D0a8BFb56424b27A5E1314E4458829"
+  const nftAddress = "0xdF9Bcdc7e7c6de0E380a0854B154f3766Be9D00E"
 
 
   useEffect(() => {
